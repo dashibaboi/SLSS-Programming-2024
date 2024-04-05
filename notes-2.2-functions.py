bdc6da423ed490fdc70fbb0eccd101dbd754e22d
Alex Qiu <alexanderqiu1984@gmail.com>
2024-04-05 10:13:50 -0700

    Star code

--- notes-2.2-functions.py
+++ notes-2.2-functions.py
@@ -1,2 +1,15 @@
 #alex
 #apr 3
+
+#greeting = "hello"
+#print(greeting * 10)
+
+##print("the quick brown fox jumps over the lazy dog" * 3)
+
+def stars(star):
+    if star < 0:
+        return "I can't do that"
+    else:
+        return "*" * star
+
+print(stars(20))
