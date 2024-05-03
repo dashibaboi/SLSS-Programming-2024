650c9c6ba07001482a76c6cdc714ddd0bf3e343d
Alex Qiu <alexanderqiu1984@gmail.com>
2024-05-03 11:35:04 -0700

    Snowflakes!!!

---
+++ pygame-excerise-snow.py
@@ -0,0 +1,101 @@
+import random
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+
+class Snowflake(pg.sprite.Sprite):
+    def __init__(self, size: int, speed: int, x_speed : int):
+        """
+        Params:
+            size: diameter of snowflake in pixels
+        """
+        # Super class constructor
+        super().__init__()
+
+        # Create a blank surface
+        self.image = pg.Surface((size, size))
+
+        # Draw a circle inside of it
+        pg.draw.circle(self.image, WHITE, (size // 2, size // 2), size // 2)
+
+        self.rect = self.image.get_rect()
+
+        # Spawn it in the middle of the screen
+        # Chooses a random x-coordinate
+        self.rect.centerx = random.randrange(0, WIDTH + 1)
+     
+
+        self.vel_x = x_speed
+        self.vel_y = speed
+
+    def update(self):
+        self.rect.y += self.vel_y
+        self.rect.x += self.vel_x
+   
+
+
+
+
+def start():
+    """Environment Setup and Game Loop"""
+
+    pg.init()
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+
+    # Add 100 snowflakes to the all_sprites Group
+    for _ in range(10):
+        all_sprites.add(Snowflake(10,5,0))
+
+    pg.display.set_caption("Snowfall Landscape")
+
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+        for _ in range(2):
+            all_sprites.add(Snowflake(random.randrange(6,11), random.randrange(3,8), random.randrange(-1,1)))
+        # --- Update the world state
+        # Update the state of all sprites
+        all_sprites.update()
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        # Draw all the sprites
+        all_sprites.draw(screen)
+
+        # Update the screen with anything new
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
